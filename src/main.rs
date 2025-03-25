f67cae46e30f18ca76dde3ee28fff30a62e8ab96
jagoum <jagoum24@gmail.com>
2025-03-25 03:47:44 +0100

    using our gcd to compute the gcd of multiple numbers

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
+use std::env;
 
-fn gcd(mut a: u128, mut b: u128) -> u128 { //   12 4 
-    // if b > 0 {
-    //     gcd(b, a % b)
-    // } else {
-    //     a
-    // }
-    while b > 0 {
-        a = b;
-        b = a % b;
+fn gcd( a: u128, b: u128) -> u128 {
+    
+    if b > 0 {
+        gcd(b, a % b)
+    } else {
+        a
     }
-    a
+    // while b > 0 {
+    //     a = b;
+    //     b = a % b;
+    // }
+    // a
 }
+#[allow(dead_code)]
 /// This function is to calculate the factorial of  a number
 fn factorial(n: u128) -> u128 {
     let mut fact = 1;
@@ -20,15 +23,17 @@ fn factorial(n: u128) -> u128 {
     fact
 }
 #[test]
-fn test_factorial(){
-    
+fn test_factorial() {
     assert_eq!(24, factorial(4))
 }
 
+#[allow(dead_code)]
 fn fizzbuzz(a: u32) -> u32 {
     let _ = a;
     a
 }
+
+#[allow(dead_code)]
 ///The Collatz Sequence is defined as follows, for an arbitrary n1 greater than zero:
 ///- If ni is 1, then the sequence terminates at ni.
 ///- If ni is even, then ni+1 = ni / 2.
@@ -42,29 +47,18 @@ fn collatz_sequence(mut n1: u128) -> u128 {
     arr
 }
 fn main() {
-    let x1 = 128;
-    let x2 = 7;
-    dbg!(gcd(x1, x2));
-    let x1 = factorial(x2);
-    // gracefully handling errors
-    fizzbuzz(match x1.try_into() {
-        Ok(good) => good,
-        Err(_) => {
-            eprintln!("Please enter a u32 integer ");
-            1
-        }
-    });
-
-    println!(
-        "Result of Collatz sequence of {x2} is {:?}",
-        collatz_sequence(match x2.try_into() {
-            Ok(good) => {
-                good
-            }
-            Err(_) => {
-                eprintln!("Please enter a valid number");
-                return;
-            }
-        })
-    );
+    let mut numbers = Vec::new();
+    for arg in env::args().skip(1) {
+        let arg: u64 = arg.trim().parse().expect("Error parsing argument");
+        numbers.push(arg);
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d.into(), (*m).into()) as u64;
+    }
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
+}
 }
